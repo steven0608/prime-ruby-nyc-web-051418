071859a85fecb3312e164d2447d1b0b41b895af9
Steven Chen <ysteven.chen2@gmail.com>
2018-05-03 23:24:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
-def prime?(i)
-for x in (-i..i)
-  if i>1 && x >=2 && !(i % x ==0)
-    true
-  else
-    false
+  def prime?(i)
+  (-i..i).each do |x|
+    if i>1 && x >=2 && !(i % x ==0)
+      true
+    else
+      false
+    end
   end
-end
-end
+  end
