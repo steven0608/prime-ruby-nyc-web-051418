1261702f6fce38b4d276fdf45112c3be35f78a01
Steven Chen <ysteven.chen2@gmail.com>
2018-05-03 22:18:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(i)
 !(i % 2 == 0|| i % 3 == 0 || i % 5 == 0 || i<0)|| i == 2 || i == 3 ? true : false
-puts "Be sure to account for negative numbers!" if (i<0)
+
 
 end
