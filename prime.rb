50589bb05f5c0063778ecb15e4f0338c1b07b68d
Steven Chen <ysteven.chen2@gmail.com>
2018-05-03 22:01:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,7 @@
 # Add  code here!
 def prime?(i)
-!(i % 2== 0|| i % 3== 0 || i % 5 == 0) ? true : false
+!(i % 2== 0|| i % 3== 0 || i % 5 == 0 || !2 || !3) ? true : false
 end
+
+i=5
+!(i % 2== 0|| i % 3== 0 || i % 5 == 0) ? true : false
