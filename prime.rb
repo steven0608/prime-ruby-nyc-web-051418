ab92a743cd8ca0ce596b6fc2627a3b21d9414222
Steven Chen <ysteven.chen2@gmail.com>
2018-05-03 22:49:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 def prime?(i)
-  if i<0
+for integer in 2..(i-1)
+  if i % integer ==0
+    false
+  elsif i<0
     puts "Be sure to account for negative numbers!"
-  else for integer in 2..(i-1)
-false if i % integer==0
+  else
+    true
+  end
 end
-end
-true
 end
