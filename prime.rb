10749cb79af13cd7ff18e578675ab2809f7f9bb5
Steven Chen <ysteven.chen2@gmail.com>
2018-05-03 22:42:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(i)
   for integer in 2..(i-1)
-!(i % 2 == 0|| i % 3 == 0 || i % 5 == 0 || i<2)|| i == 2 || i == 3 ? true : false
+!(i % integer) ? true : false
 end
 
 end
